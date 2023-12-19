e97311e3de138df3c7d0db6df6861c18f2d9ef56
chungdi <vicn1708@gmail.com>
2023-12-19 13:02:32 +0700

    ssl

--- conf.sh
+++ conf.sh
@@ -1,3 +1,5 @@
 bash ./jenkins/jenkins.sh
 
 sudo docker compose up -d
+
+sudo docker compose logs certbot
--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -24,7 +24,7 @@ sudo docker run \
   --env DOCKER_HOST=tcp://docker:2376 \
   --env DOCKER_CERT_PATH=/certs/client \
   --env DOCKER_TLS_VERIFY=1 \
-  --publish 8080:8080 \
+  --publish 8081:8081 \
   --publish 50000:50000 \
   --volume jenkins-data:/var/jenkins_home \
   --volume jenkins-docker-certs:/certs/client:ro \
