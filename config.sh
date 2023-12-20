c386f6d578c89d878c05955d41698f47a11da108
chungdi <vicn1708@gmail.com>
2023-12-20 14:42:59 +0700

    z

--- config.sh
+++ config.sh
@@ -1,18 +1,6 @@
 echo "Install Docker..."
 bash docker/install.sh
 
-echo "Create docker network..."
-sudo docker network create nevent-network
-
-echo "SSL nevent.io.vn"
-sudo docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ --email vicn1708@gmail.com --agree-tos --no-eff-email --staging -d nevent.io.vn -d www.nevent.io.vn
-
-echo "SSL admin.nevent.io.vn"
-sudo docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ --email vicn1708@gmail.com --agree-tos --no-eff-email --staging -d admin.nevent.io.vn -d www.admin.nevent.io.vn
-
-echo "SSL server.nevent.io.vn"
-sudo docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ --email vicn1708@gmail.com --agree-tos --no-eff-email --staging -d server.nevent.io.vn -d www.server.nevent.io.vn
-
 echo "RUN services"
 sudo docker compose up -d
 
