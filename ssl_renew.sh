bc1412b34f9057d30fc16b052d4d69cfee2bafee
chungdi <vicn1708@gmail.com>
2023-12-20 08:25:06 +0700

    Create ssl_renew.sh

---
+++ ssl_renew.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+COMPOSE="/usr/local/bin/docker-compose --ansi never"
+DOCKER="/usr/bin/docker"
+
+$COMPOSE run certbot renew --dry-run && $COMPOSE kill -s SIGHUP webserver
+$DOCKER system prune -af
