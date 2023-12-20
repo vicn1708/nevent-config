fe1e867f891f4b3053659ca33325064a25655d6e
chungdi <vicn1708@gmail.com>
2023-12-20 13:52:44 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,28 +1,28 @@
-echo "\nSSL nevent.io.vn\n"
+echo "SSL nevent.io.vn"
 sudo docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ --email vicn1708@gmail.com --agree-tos --no-eff-email --staging -d nevent.io.vn -d www.nevent.io.vn
 
-echo "\nSSL admin.nevent.io.vn\n"
+echo "SSL admin.nevent.io.vn"
 sudo docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ --email vicn1708@gmail.com --agree-tos --no-eff-email --staging -d admin.nevent.io.vn -d www.admin.nevent.io.vn
 
-echo "\nSSL server.nevent.io.vn\n"
+echo "SSL server.nevent.io.vn"
 sudo docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ --email vicn1708@gmail.com --agree-tos --no-eff-email --staging -d server.nevent.io.vn -d www.server.nevent.io.vn
 
-echo "\nRUN services\n"
+echo "RUN services"
 sudo docker compose up -d
 
-echo "\nRenew SSL nevent.io.vn\n"
+echo "Renew SSL nevent.io.vn"
 sudo docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ --email vicn1708@gmail.com --agree-tos --no-eff-email --force-renewal -d nevent.io.vn -d www.nevent.io.vn
 
-echo "\nRenew SSL admin.nevent.io.vn\n"
+echo "Renew SSL admin.nevent.io.vn"
 sudo docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ --email vicn1708@gmail.com --agree-tos --no-eff-email --force-renewal -d admin.nevent.io.vn -d www.admin.nevent.io.vn
 
-echo "\nRenew SSL server.nevent.io.vn\n"
+echo "Renew SSL server.nevent.io.vn"
 sudo docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ --email vicn1708@gmail.com --agree-tos --no-eff-email  --force-renewal -d server.nevent.io.vn -d www.server.nevent.io.vn
 
-echo "\nStop NGINX\n"
+echo "Stop NGINX"
 sudo docker compose stop webserver
 
-echo "\nConfig openSSL\n"
+echo "Config openSSL"
 sudo openssl dhparam -out ./nginx/dhparam/dhparam-2048.pem 2048
 
 sudo cp -r ./nginx/conf-ssl/* ./nginx/conf
