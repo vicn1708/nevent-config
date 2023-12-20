aa22f5c15027e88315e335a6aad871bf8f9bf472
chungdi <vicn1708@gmail.com>
2023-12-20 13:59:18 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,7 +1,8 @@
+echo "Install Docker..."
+bash docker/install.sh
+
 echo "SSL nevent.io.vn"
 sudo docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ --email vicn1708@gmail.com --agree-tos --no-eff-email --staging -d nevent.io.vn -d www.nevent.io.vn
-
-echo "Install Docker..."
 
 echo "SSL admin.nevent.io.vn"
 sudo docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ --email vicn1708@gmail.com --agree-tos --no-eff-email --staging -d admin.nevent.io.vn -d www.admin.nevent.io.vn
