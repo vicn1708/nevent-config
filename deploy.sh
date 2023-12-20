cf79f7bbec4ccc186e18fdf11d7ddabbc788d5eb
chungdi <vicn1708@gmail.com>
2023-12-20 17:24:36 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
-sudo docker compoes pull nevent-client
-sudo docker compoes pull nevent-admin
-sudo docker compoes pull nevent-server
+sudo docker compose pull nevent-client
+sudo docker compose pull nevent-admin
+sudo docker compose pull nevent-server
 
-sudo docker compoes up -d nevent-client
-sudo docker compoes up -d nevent-admin
-sudo docker compoes up -d nevent-server
+sudo docker compose up -d nevent-client
+sudo docker compose up -d nevent-admin
+sudo docker compose up -d nevent-server
 
