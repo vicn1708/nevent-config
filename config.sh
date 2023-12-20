74b53e646d6c697728967fe3daa0320a394b5646
chungdi <vicn1708@gmail.com>
2023-12-20 15:38:53 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,25 +2,13 @@ echo "Install Docker..."
 bash docker/install.sh
 
 echo "SSL nevent.io.vn"
-sudo docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ --email vicn1708@gmail.com --agree-tos --no-eff-email --staging -d nevent.io.vn -d www.nevent.io.vn
-
-echo "SSL admin.nevent.io.vn"
-sudo docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ --email vicn1708@gmail.com --agree-tos --no-eff-email --staging -d admin.nevent.io.vn -d www.admin.nevent.io.vn
-
-echo "SSL server.nevent.io.vn"
-sudo docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ --email vicn1708@gmail.com --agree-tos --no-eff-email  --staging -d server.nevent.io.vn -d www.server.nevent.io.vn
+sudo docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ --email vicn1708@gmail.com --agree-tos --no-eff-email --staging -d nevent.io.vn -d www.nevent.io.vn -d admin.nevent.io.vn -d www.admin.nevent.io.vn -d server.nevent.io.vn -d www.server.nevent.io.vn
 
 echo "RUN services"
 sudo docker compose up -d
 
 echo "Renew SSL nevent.io.vn"
-sudo docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ --email vicn1708@gmail.com --agree-tos --no-eff-email --force-renewal -d nevent.io.vn -d www.nevent.io.vn
-
-echo "Renew SSL admin.nevent.io.vn"
-sudo docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ --email vicn1708@gmail.com --agree-tos --no-eff-email --force-renewal -d admin.nevent.io.vn -d www.admin.nevent.io.vn
-
-echo "Renew SSL server.nevent.io.vn"
-sudo docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ --email vicn1708@gmail.com --agree-tos --no-eff-email  --force-renewal -d server.nevent.io.vn -d www.server.nevent.io.vn
+sudo docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ --email vicn1708@gmail.com --agree-tos --no-eff-email --force-renewal -d nevent.io.vn -d www.nevent.io.vn -d admin.nevent.io.vn -d www.admin.nevent.io.vn -d server.nevent.io.vn -d www.server.nevent.io.vn
 
 echo "Stop NGINX"
 sudo docker compose stop webserver
