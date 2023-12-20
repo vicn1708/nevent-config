d9bc8bf0ba49e588385f8689567a3a132c5a4cc8
chungdi <vicn1708@gmail.com>
2023-12-20 14:03:35 +0700

    Update install.sh

--- docker/install.sh
+++ docker/install.sh
@@ -4,7 +4,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
 sudo apt install docker-ce
-sudo systemctl status docker
+# sudo systemctl status docker
 sudo usermod -aG docker nevent
 su - nevent
 groups
