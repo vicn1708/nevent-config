b8e19e031bf546af9d7ab72cb1e46929567451f4
chungdi <vicn1708@gmail.com>
2023-12-20 19:23:47 +0700

    c

--- config.sh
+++ config.sh
@@ -1,8 +1,8 @@
 echo "Install Docker..."
 bash docker/install.sh
 
-echo "Install Jenkins..."
-bash jenkins/install.sh
+# echo "Install Jenkins..."
+# bash jenkins/install.sh
 
 echo "SSL nevent.io.vn"
 sudo docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ --email vicn1708@gmail.com --agree-tos --no-eff-email --staging -d nevent.io.vn -d www.nevent.io.vn 
@@ -13,8 +13,8 @@ sudo docker compose run --rm  certbot certonly --webroot --webroot-path /var/www
 echo "SSL server.nevent.io.vn"
 sudo docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ --email vicn1708@gmail.com --agree-tos --no-eff-email --staging -d server.nevent.io.vn -d www.server.nevent.io.vn
 
-echo "SSL jenkins.nevent.io.vn"
-sudo docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ --email vicn1708@gmail.com --agree-tos --no-eff-email --staging -d jenkins.nevent.io.vn -d www.jenkins.nevent.io.vn
+# echo "SSL jenkins.nevent.io.vn"
+# sudo docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ --email vicn1708@gmail.com --agree-tos --no-eff-email --staging -d jenkins.nevent.io.vn -d www.jenkins.nevent.io.vn
 
 echo "RUN services"
 sudo docker compose up -d
@@ -36,8 +36,8 @@ sudo docker compose run --rm  certbot certonly --webroot --webroot-path /var/www
 echo "Renew SSL server.nevent.io.vn"
 sudo docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ --email vicn1708@gmail.com --agree-tos --no-eff-email --force-renewal -d server.nevent.io.vn -d www.server.nevent.io.vn
 
-echo "Renew SSL jenkins.nevent.io.vn"
-sudo docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ --email vicn1708@gmail.com --agree-tos --no-eff-email --force-renewal -d jenkins.nevent.io.vn -d www.jenkins.nevent.io.vn
+# echo "Renew SSL jenkins.nevent.io.vn"
+# sudo docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ --email vicn1708@gmail.com --agree-tos --no-eff-email --force-renewal -d jenkins.nevent.io.vn -d www.jenkins.nevent.io.vn
 
 echo "Stop NGINX"
 sudo docker compose down webserver
