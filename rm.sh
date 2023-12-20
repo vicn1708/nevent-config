37afc3cf67a6545c611d6522cfad6e4fcc913fc4
chungdi <vicn1708@gmail.com>
2023-12-20 16:31:19 +0700

    Update rm.sh

--- rm.sh
+++ rm.sh
@@ -1,7 +1,6 @@
 sudo docker stop $(docker ps -qa)
 sudo docker rm $(docker ps -qa)
-sudo docker rmi $(docker images -qa)
-sudo docker rmi $(docker images -qa)
+# sudo docker rmi $(docker images -qa)
 
 sudo docker network prune -f
 sudo docker volume prune -f
